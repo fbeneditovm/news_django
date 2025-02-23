89d4fa9e4ec7c00071ad5b249853899670568ccf
fmagalhaes <fmagalhaes@druid.com.br>
2025-02-23 20:15:30 -0300

    Separate Users and News APIs

---
+++ utils/global_values.py
@@ -0,0 +1,16 @@
+USER_TYPES = [
+        ('EMP', 'Employee'),
+        ('CLI', 'Client'),
+    ]
+ARTICLE_STATUS = [
+        ('DRAF', 'Draft'),
+        ('PUBD', 'Published'),
+    ]
+ARTICLE_COLUMNS = [
+        ('MAIN', 'Main'),
+        ('POW', 'Power'),
+        ('TAX', 'Taxes'),
+        ('HLTH', 'Health'),
+        ('EN', 'Energy'),
+        ('LAB', 'Labor'),
+    ]
