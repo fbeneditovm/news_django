e616eccdc0f470e4fc06d8f48d6205c1c8ca2b16
fmagalhaes <fmagalhaes@druid.com.br>
2025-02-24 10:08:59 -0300

    Run black

--- news_api/apps.py
+++ news_api/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class NewsApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'news_api'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "news_api"
--- users_api/urls.py
+++ users_api/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import ClientUserCreateView, UserManagementView, EmailTokenObtainPairView, UserCollectionView  # Add this import
+from .views import (
+    ClientUserCreateView,
+    UserManagementView,
+    EmailTokenObtainPairView,
+    UserCollectionView,
+)  # Add this import
 
 # from .views import UserViewSet
 
@@ -14,9 +19,7 @@ urlpatterns = [
     path("token/", EmailTokenObtainPairView.as_view(), name="token_obtain_pair"),  # Use custom view
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("self_register/", ClientUserCreateView.as_view(), name="client_user_create"),  # POST to create client user
-    path(
-        "users/", UserCollectionView.as_view(), name="user_management"
-    ),  
+    path("users/", UserCollectionView.as_view(), name="user_management"),
     path(
         "users/<int:user_id>/", UserManagementView.as_view(), name="user_management"
     ),  # PUT and DELETE for user management
