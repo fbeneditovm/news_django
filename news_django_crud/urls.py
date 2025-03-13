60efa94fd82b01206e7553e5431c1d5b5d66df8b
fmagalhaes <fmagalhaes@druid.com.br>
2025-03-13 13:59:33 -0300

    Flake8 and Black

--- news_django_crud/urls.py
+++ news_django_crud/urls.py
@@ -24,7 +24,6 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
 import os
-from rest_framework_simplejwt.authentication import JWTAuthentication
 from users_api.authentication import BearerJWTAuthentication
 
 schema_view = get_schema_view(
